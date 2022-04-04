46f676cc21509971b525ea146e6c42f7ab2e8e87
Sakshi Goyal <sakshi.goyal@dianapps.com>
2022-04-04 14:50:38 +0530

    update

---
+++ pr.js
@@ -0,0 +1,9 @@
+function convertString() {
+    let word = 'the brown dog'
+    let part = word.slice(0)
+    let firstLetter = toupperscript.word[0]
+
+    console.log(`${part} + '' + ${firstLetter}` )
+}
+const outPrint = convertString()
+console.log(outPrint)
